d18ad24546da505eb31176e183c5a4e48267cabf
Awsaf Alam <awsafalam@gmail.com>
2017-10-13 15:08:31 +0600

    Add files via upload

---
+++ Online.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+
+using namespace std;
+
+
+int main(){
+
+    int V, n , c,d;
+    vector<int> coin, denomination;
+
+    scanf("%d%d",&V, &n);
+    for(int i=0 ; i< n ; i++){
+        scanf("%d%d",&c , &d );
+        coin.push_back(c);
+        denomination.push_back(d);
+    }
+
+
+
+
+
+}
